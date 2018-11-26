fb189801e1ba79dedeca4446d33e1f4124c8b2c5
gilneyjr <gilneyjnr@gmail.com>
2018-11-26 18:31:09 -0300

    Creating initial Spring project

---
+++ src/main/java/com/lab/dataest/DataestApplication.java
@@ -0,0 +1,12 @@
+package com.lab.dataest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lab/dataest/DataestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lab.dataest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
